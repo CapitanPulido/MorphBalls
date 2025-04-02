adac5ff3e1997f9cb58878884fdc1d53332e35be
CapitanPulido <carlosdanielpulidom2@gmail.com>
2025-04-01 18:59:57 -0600

    OtroHoy

---
+++ Assets/Scripts/Caja2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caja2 : MonoBehaviour
+{
+
+    PolyMorph morph;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(morph.Metal)
+        {
+
+        }
+    }
+
+
+}
