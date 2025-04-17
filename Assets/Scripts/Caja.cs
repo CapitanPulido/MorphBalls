5c1c3bb6e9a3380d97ec00f19e867c2301801e86
CapitanPulido <carlosdanielpulidom2@gmail.com>
2025-04-17 12:55:17 -0600

    si

--- Assets/Scripts/Caja.cs
+++ Assets/Scripts/Caja.cs
@@ -9,11 +9,16 @@ public class Caja : MonoBehaviour
     public Rigidbody2D Rb;
     public Rigidbody2D Rb2;
 
+    public GameObject parteIzquierda;
+    public GameObject parteDerecha;
+    public float tiempoParaDestruir = 3f;
+    public float fuerzaSeparacion = 5f;
+    private bool yaSeDividio = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        Rb.mass = 1000;
-        Rb2.mass = 1000;
+
     }
 
     // Update is called once per frame
@@ -24,11 +29,37 @@ public class Caja : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if (yaSeDividio) return;
+
         if (morph.Metal && collision.gameObject.CompareTag("Pelota"))
         {
-            Rb.mass = 1;
-            Rb2.mass = 1;
             Debug.Log("choque");
+
+            yaSeDividio = true;
+            DividirPared();
         }
+    }
+
+    void DividirPared()
+    {
+        // Separar las piezas y activar gravedad
+        Rigidbody2D rbIzq = parteIzquierda.GetComponent<Rigidbody2D>();
+        Rigidbody2D rbDer = parteDerecha.GetComponent<Rigidbody2D>();
+
+        rbIzq.bodyType = RigidbodyType2D.Dynamic;
+        rbDer.bodyType = RigidbodyType2D.Dynamic;
+
+        // Separarlas con una fuerza
+        rbIzq.AddForce(Vector2.left * fuerzaSeparacion, ForceMode2D.Impulse);
+        rbDer.AddForce(Vector2.right * fuerzaSeparacion, ForceMode2D.Impulse);
+
+        // Opcional: desactivar el objeto padre
+        GetComponent<Collider2D>().enabled = false;
+
+        // Destruir las piezas después de un tiempo
+        Destroy(parteIzquierda, tiempoParaDestruir);
+        Destroy(parteDerecha, tiempoParaDestruir);
+        Destroy(gameObject, tiempoParaDestruir + 0.1f); // Limpieza del objeto padre
     }
 }
